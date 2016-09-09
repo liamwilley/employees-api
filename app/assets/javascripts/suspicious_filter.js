04f873e15a5056f002f9310570f8511a0c6d1e40
Ryan Meier <rymei10@gmail.com>
2016-09-08 21:21:49 -0700

    suspiciousfilter in vendor

--- app/assets/javascripts/suspicious_filter.js
+++
@@ -1,7 +0,0 @@
-angular.module("app").filter('suspicious', [function () {
-  return function(employees) {
-    if (employees) {
-      return [employees[6]];
-    }
-  }
-}]);
---
+++ vendor/assets/javascripts/suspicious_filter.js
@@ -0,0 +1,19 @@
+angular.module("suspiciousEmployees", []).filter('suspicious', [function () {
+  return function(employees) {
+    if (employees) {
+      var suspiciousEmployees = [];
+
+      var date = new Date();
+      var thisYear = date.getFullYear();
+      var seventyYearsAgo = thisYear - 70;
+
+      for(var i=0;i<employees.length;i++) {
+        var birthYear = parseInt(employees[i].birthdate.substring(0,4));
+        if (birthYear < seventyYearsAgo) {
+          suspiciousEmployees.push(employees[i]);
+        };
+      }
+      return suspiciousEmployees;
+    }
+  }
+}]);
